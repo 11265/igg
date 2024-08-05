3d4c7e94ae684583fad016951a7091d6d53910f0
11265 <xdllll@163.com>
2024-08-05 13:53:42 +0800

    创建 UIConstants.m

---
+++ Utilities/UIConstants.m
@@ -0,0 +1,9 @@
+// UIConstants.m
+
+#import "UIConstants.h"
+
+const CGFloat kTopBarHeight = 45.0f; // 定义顶部栏的高度为 45 像素
+const CGFloat kFloatingButtonSize = 50.0f; // 定义浮动按钮的尺寸为 50 像素
+const NSInteger kTopBarButtonCount = 6; // 定义顶部栏中按钮的数量为 6 个
+const NSTimeInterval kLayoutUpdateInterval = 9.0; // 定义布局更新的时间间隔为 9 秒
+const char* TARGET_PROCESS_NAME = "pvz"; // 定义目标进程的名称为 "pvz"
