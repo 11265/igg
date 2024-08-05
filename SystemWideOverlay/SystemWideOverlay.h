59133ba980db5a8a91b2483d661137bb9ee305e8
11265 <xdllll@163.com>
2024-08-05 14:28:44 +0800

    更新 SystemWideOverlay.h

--- SystemWideOverlay/SystemWideOverlay.h
+++ SystemWideOverlay/SystemWideOverlay.h
@@ -1,24 +1,46 @@
 // SystemWideOverlay.h
 
 #import <UIKit/UIKit.h>
-#import "JHDragView/JHDragView.h"
 
-@interface SystemWideOverlay : UIWindow
+@interface SystemWideOverlay : UIWindow <UITableViewDataSource, UITableViewDelegate>
 
-@property (nonatomic, strong, readonly) JHDragView *floatingButton;
-@property (nonatomic, assign, readwrite) BOOL isMenuOpen;
+@property (nonatomic, strong) UIView *menuContainerView;
+@property (nonatomic, strong) UIView *contentView;
+@property (nonatomic, strong) NSArray *pageViews;
+@property (nonatomic, assign) NSInteger currentPageIndex;
+@property (nonatomic, assign) BOOL isMenuOpen;
+@property (nonatomic, strong) UIButton *floatingButton;
+@property (nonatomic, assign) CGPoint initialFloatingButtonPosition;
+@property (nonatomic, assign) CGRect desktopBounds;
 
-@property (nonatomic, strong, readwrite) UIView *menuContainerView;
-@property (nonatomic, strong, readwrite) UIView *contentView;
-@property (nonatomic, strong, readwrite) NSArray<UIView *> *pageViews;
-@property (nonatomic, assign, readwrite) NSInteger currentPageIndex;
-@property (nonatomic, assign, readwrite) CGRect desktopBounds;
-@property (nonatomic, assign, readwrite) CGPoint initialFloatingButtonPosition;
+- (void)setupMenuView;
+- (void)setupTopBar:(CGRect)screenBounds;
+- (void)setupTopBarButtons:(UIView *)topBar;
+- (void)setupContentView:(CGRect)screenBounds;
+- (void)setupPageViews;
+- (void)updatePageVisibility;
+- (void)updateTopBarButtonsAppearance;
+- (UIView *)createPageViewWithFrame:(CGRect)frame title:(NSString *)title;
+- (void)addSettingsContent:(UIView *)pageView;
+- (void)addSearchContent:(UIView *)pageView;
+- (void)addProcessContent:(UIView *)pageView; // 添加这一行
+- (UIView *)createCustomAlertViewWithTitle:(NSString *)title message:(NSString *)message;
+- (void)topBarButtonTapped:(UIButton *)sender;
+- (void)dismissCustomAlert:(UIButton *)sender;
+- (void)searchButtonTapped:(UIButton *)sender;
+- (void)squareButtonTapped:(UIButton *)sender;
+- (void)closeMenuFromCategory;
 
-+ (instancetype)sharedInstance;
-- (void)toggleMenu;
-- (void)closeMenu;
-- (void)updateLayout;
-- (void)checkAndCloseMenuIfNeeded;
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)tableView:(UITableView *)tableView didHighlightRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)tableView:(UITableView *)tableView didUnhighlightRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)handleLongPress:(UILongPressGestureRecognizer *)gestureRecognizer;
 
 @end
