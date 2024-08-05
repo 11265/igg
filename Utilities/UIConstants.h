41419d739491908c7f22e4a1fb492c749a552db3
11265 <xdllll@163.com>
2024-08-05 13:53:25 +0800

    创建 UIConstants.h

---
+++ Utilities/UIConstants.h
@@ -0,0 +1,10 @@
+// UIConstants.h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+extern const CGFloat kTopBarHeight;
+extern const CGFloat kFloatingButtonSize;
+extern const NSInteger kTopBarButtonCount;
+extern const NSTimeInterval kLayoutUpdateInterval;
+extern const char* TARGET_PROCESS_NAME;
