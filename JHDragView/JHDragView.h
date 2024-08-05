a60bc3eb26f8327241fbc91908c503518731805b
11265 <xdllll@163.com>
2024-08-05 13:54:15 +0800

    创建 JHDragView.h

---
+++ JHDragView/JHDragView.h
@@ -0,0 +1,13 @@
+// JHDragView.h
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JHDragView : UIView
+
+@property (nonatomic, assign) BOOL isDragging;
+
+@end
+
+NS_ASSUME_NONNULL_END
