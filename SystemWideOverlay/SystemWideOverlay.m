65e4927d39ed534899356999d08d7dab535698f3
11265 <xdllll@163.com>
2024-08-07 17:48:39 +0800

    Update SystemWideOverlay.m

--- SystemWideOverlay/SystemWideOverlay.m
+++ SystemWideOverlay/SystemWideOverlay.m
@@ -8,7 +8,7 @@
 #import <SpringBoard/SpringBoard.h>
 #import <SpringBoard/SBApplication.h>
 
-@interface SystemWideOverlay () <UITableViewDataSource, UITableViewDelegate>
+@interface SystemWideOverlay ()
 
 @property (nonatomic, strong) NSTimer *layoutUpdateTimer;
 
@@ -35,7 +35,7 @@
 
 - (void)commonInit {
     [LogManager log:@"Initializing SystemWideOverlay"];
-
+    
     self.isMenuOpen = NO;
     self.menuContainerView.hidden = YES;
 
@@ -46,11 +46,11 @@
     [self addSubviews];
     [self makeKeyAndVisible];
     [self registerForNotifications];
-
+    
     self.desktopBounds = [self getDesktopBounds];
     [self updateLayout];
     [self setupLayoutUpdateTimer];
-
+    
     [LogManager log:@"SystemWideOverlay initialization completed"];
 }
 
@@ -70,16 +70,16 @@
     CGFloat buttonSize = kFloatingButtonSize * 0.7;
     CGFloat initialX = 40;
     CGFloat initialY = 20;
-
+    
     self.initialFloatingButtonPosition = CGPointMake(initialX, initialY);
-
+    
     _floatingButton = [[JHDragView alloc] initWithFrame:CGRectMake(initialX, initialY, buttonSize, buttonSize)];
     _floatingButton.userInteractionEnabled = YES;
     [_floatingButton addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(toggleMenu)]];
-
+    
     _floatingButton.layer.cornerRadius = buttonSize / 2;
     _floatingButton.clipsToBounds = YES;
-
+    
     [LogManager log:@"Floating button set up at (%f, %f)", initialX, initialY];
 }
 
@@ -98,10 +98,10 @@
 }
 
 - (void)setupLayoutUpdateTimer {
-    self.layoutUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:kLayoutUpdateInterval
-                                                              target:self
-                                                            selector:@selector(updateLayoutFromTimer)
-                                                            userInfo:nil
+    self.layoutUpdateTimer = [NSTimer scheduledTimerWithTimeInterval:kLayoutUpdateInterval 
+                                                              target:self 
+                                                            selector:@selector(updateLayoutFromTimer) 
+                                                            userInfo:nil 
                                                              repeats:YES];
 }
 
@@ -110,11 +110,11 @@
         [LogManager log:@"On home screen, menu toggle not allowed"];
         return;
     }
-
+    
     self.isMenuOpen = !self.isMenuOpen;
     self.menuContainerView.hidden = !self.isMenuOpen;
     [LogManager log:@"Menu toggled - Is open: %@", self.isMenuOpen ? @"Yes" : @"No"];
-
+    
     if (self.isMenuOpen) {
         [self updateLayout];
     }
@@ -166,10 +166,10 @@
 - (void)stopOngoingOperations {
     [self.layoutUpdateTimer invalidate];
     self.layoutUpdateTimer = nil;
-
+    
     [self.floatingButton.layer removeAllAnimations];
     [self.menuContainerView.layer removeAllAnimations];
-
+    
     [LogManager log:@"All ongoing operations stopped"];
 }
 
@@ -190,18 +190,18 @@
 
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
     UIView *hitView = [super hitTest:point withEvent:event];
-
+    
     if (hitView == self.rootViewController.view) {
         [LogManager log:@"Hit test on root view controller view, returning nil"];
         return nil;
     }
-
+    
     if (hitView == self.floatingButton || [hitView isDescendantOfView:self.floatingButton] ||
         (!self.menuContainerView.hidden && (hitView == self.menuContainerView || [hitView isDescendantOfView:self.menuContainerView]))) {
         [LogManager log:@"Hit test on floating button or menu container"];
         return hitView;
     }
-
+    
     [LogManager log:@"Hit test returned nil"];
     return nil;
 }
@@ -214,72 +214,6 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     [self.layoutUpdateTimer invalidate];
     [LogManager log:@"SystemWideOverlay deallocated"];
-}
-
-#pragma mark - UITableViewDataSource
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 20;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SearchCell" forIndexPath:indexPath];
-
-    cell.textLabel.text = [NSString stringWithFormat:@"搜索结果 %ld", (long)indexPath.row + 1];
-
-    return cell;
-}
-
-#pragma mark - UITableViewDelegate
-
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Remove the default selection style
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-
-    // Set the background color of the cell
-    cell.backgroundColor = [UIColor clearColor];
-
-    // Update the text color and font size
-    cell.textLabel.textColor = [UIColor whiteColor];
-    cell.textLabel.font = [UIFont systemFontOfSize:14]; // Increased font size for better readability
-
-    // Add a custom selected background view
-    UIView *selectedBackgroundView = [[UIView alloc] init];
-    selectedBackgroundView.backgroundColor = [UIColor colorWithRed:0.455 green:0.792 blue:0.933 alpha:1.0]; // #74CAEC
-    cell.selectedBackgroundView = selectedBackgroundView;
-}
-
-- (void)tableView:(UITableView *)tableView didHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    cell.contentView.backgroundColor = [UIColor colorWithRed:0.455 green:0.792 blue:0.933 alpha:1.0]; // #74CAEC
-    cell.textLabel.textColor = [UIColor whiteColor]; // 确保文字在高亮状态下仍然可见
-}
-
-- (void)tableView:(UITableView *)tableView didUnhighlightRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    cell.contentView.backgroundColor = [UIColor clearColor];
-    cell.textLabel.textColor = [UIColor whiteColor];
-}
-
-// 新添加的长按手势处理方法
-- (void)handleLongPress:(UILongPressGestureRecognizer *)gestureRecognizer {
-    if (gestureRecognizer.state == UIGestureRecognizerStateBegan) {
-        CGPoint point = [gestureRecognizer locationInView:gestureRecognizer.view];
-        NSIndexPath *indexPath = [(UITableView *)gestureRecognizer.view indexPathForRowAtPoint:point];
-
-        if (indexPath) {
-            [LogManager log:@"长按了搜索结果 %ld", (long)indexPath.row + 1];
-
-            UIView *alertView = [self createCustomAlertViewWithTitle:@"搜索结果"
-                                                             message:[NSString stringWithFormat:@"你长按了搜索结果 %ld", (long)indexPath.row + 1]];
-
-            alertView.center = self.menuContainerView.center;
-            [self.menuContainerView addSubview:alertView];
-
-            // 确保 menuContainerView 可以接收用户交互
-            self.menuContainerView.userInteractionEnabled = YES;
-        }
-    }
 }
 
 @end
